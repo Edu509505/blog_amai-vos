df0e67d4f4b36ab3cdc9c4a4fff0257f5db58e7f
Edu <eduardonascimento60650@gmail.com>
2025-01-20 13:24:03 -0300

    recuperando

---
+++ README.md
@@ -0,0 +1,1 @@
+Mais um teste para estudar o git
---
+++ index.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // esconde e mostra o btn
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 160) { // distancia que tem que rolar antes de aparecer
+            $('.go-top').fadeIn(250);
+        } else {
+            $('.go-top').fadeOut(250);
+        }
+    });
+    // manda pro topo
+    $('.go-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: 0 }, 1200);
+    });
+});
